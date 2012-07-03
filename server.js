9ab75af8f2aea9eb3ad93a63a4ad708e3b2e3259
madesst <madesst@gmail.com>
2012-07-03 22:19:23 +0400

    WIP, pivot

--- server.js
+++ server.js
@@ -13,27 +13,31 @@ var socketioserver = io.listen(httpserver).set('log level', 2);
 var REDIS_KEY = 'redis_key';
 var CHAT_PUBLIC_EXCHANGE_NAME = 'chat.public.exchange';
 var CHAT_PRIVATE_EXCHANGE_NAME = 'chat.private.exchange';
+var CHAT_TRANSFERS_EXCHANGE_NAME = 'chat.transfers.exchange';
 var CHAT_PUBLIC_QUEUE_NAME = 'chat.public';
 var CHAT_PRIVATE_QUEUE_NAME = 'chat.private';
+var CHAT_TRANSFERS_QUEUE_NAME = 'chat.transfers';
 var CHAT_AMQP_QUEUE_CONFIG = {durable: true, autoDelete: true};
 var CHAT_AMQP_CONNECTION_CONFIG = { host: 'localhost', login: 'guest', password: 'guest' };
+var CHAT_DEFAULT_USER = {nickname: 'Guest', current_room: 'someroom'};
 
 var socketUsers = [];//my redis :)
 var socketConnections = [];
 
 socketioserver.sockets.on('connection', function(socket) {
-	var public_exchange, private_exchange, current_user, amqp_connection;
+	var public_exchange, private_exchange, transfer_exchage, current_user, amqp_connection;
 
 	socket.on('link_socket', function(data) {
 		socketConnections[socket.id] = cookie.parse(data)[REDIS_KEY];
-		current_user = socketUsers[socketConnections[socket.id]] ? socketUsers[socketConnections[socket.id]] : 'Guest';
+		current_user = socketUsers[socketConnections[socket.id]] ? socketUsers[socketConnections[socket.id]] : CHAT_DEFAULT_USER;
+
 
 		amqp_connection = amqp.createConnection(CHAT_AMQP_CONNECTION_CONFIG);
 		amqp_connection.on('ready', function () {
-			amqp_connection.queue(CHAT_PUBLIC_QUEUE_NAME + '.'+ current_user + '.' + socket.id, CHAT_AMQP_QUEUE_CONFIG, function(q){
-				amqp_connection.exchange(CHAT_PUBLIC_EXCHANGE_NAME, {type: 'fanout', durable: true}, function (exchange) {
+			amqp_connection.queue(CHAT_PUBLIC_QUEUE_NAME + '.'+ current_user.nickname + '.' + socket.id, CHAT_AMQP_QUEUE_CONFIG, function(q){
+				amqp_connection.exchange(CHAT_PUBLIC_EXCHANGE_NAME, {type: 'topic', durable: true}, function (exchange) {
 					public_exchange = exchange;
-					q.bind(exchange, '#');
+					q.bind(exchange, current_user.current_room + '.#');
 					q.subscribe(function (message, headers, deliveryInfo) {
 						socket.send(JSON.stringify({
 							message: message.message,
@@ -45,10 +49,27 @@ socketioserver.sockets.on('connection', function(socket) {
 				});
 			});
 
-			amqp_connection.queue(CHAT_PRIVATE_QUEUE_NAME + '.'+ current_user + '.' + socket.id, CHAT_AMQP_QUEUE_CONFIG, function(q){
+			amqp_connection.queue(CHAT_TRANSFERS_QUEUE_NAME + '.'+ current_user.nickname + '.' + socket.id, CHAT_AMQP_QUEUE_CONFIG, function(q){
+				amqp_connection.exchange(CHAT_TRANSFERS_EXCHANGE_NAME, {type: 'topic', durable: true}, function (exchange) {
+					transfer_exchage = exchange;
+					q.bind(exchange, current_user.current_room + '.#');
+					q.subscribe(function (message, headers, deliveryInfo) {
+						//TODO: rebind public queue
+						console.log(message);
+//						socket.send(JSON.stringify({
+//							message: message.message,
+//							type: 'public',
+//							from: message.from,
+//							to: message.to
+//						}));
+					});
+				});
+			});
+
+			amqp_connection.queue(CHAT_PRIVATE_QUEUE_NAME + '.'+ current_user.nickname + '.' + socket.id, CHAT_AMQP_QUEUE_CONFIG, function(q){
 				amqp_connection.exchange(CHAT_PRIVATE_EXCHANGE_NAME, {type: 'topic', durable: true}, function (exchange) {
 					private_exchange = exchange;
-					q.bind(private_exchange, '#.'+current_user+'.#');
+					q.bind(private_exchange, '#.'+current_user.nickname+'.#');
 					q.subscribe(function (message, headers, deliveryInfo) {
 						socket.send(JSON.stringify({
 							message: message.message,
@@ -60,7 +81,7 @@ socketioserver.sockets.on('connection', function(socket) {
 				});
 
 				socket.emit('set_current_user', JSON.stringify({
-					nickname: current_user
+					nickname: current_user.nickname
 				}));
 			});
 		});
@@ -72,24 +93,31 @@ socketioserver.sockets.on('connection', function(socket) {
 			return false;
 		}
 
-		current_user = socketUsers[socketConnections[socket.id]] ? socketUsers[socketConnections[socket.id]] : 'Guest';
+		current_user = socketUsers[socketConnections[socket.id]] ? socketUsers[socketConnections[socket.id]] : CHAT_DEFAULT_USER;
 
 		var recipients = '';
 		var message = msg.message;
 		var exchanger = public_exchange;
-		var routing_key = 'some_chatroom.'+current_user;
+		var routing_key = current_user.current_room+'.'+current_user.nickname;
 
 		var type_private_check = message.match(/private\s\[(.*)\]\s(.*)/);
 		if(type_private_check){//Private message
 			recipients = type_private_check[1]; message = type_private_check[2];
-			routing_key = current_user+'.'+recipients;
+			routing_key = current_user.nickname+'.'+recipients;
 			exchanger = private_exchange;
 		} else{//Public message
 			var type_to_check = message.match(/to\s\[(.*)\]\s(.*)/);
 			if(type_to_check) { recipients = type_to_check[1]; message = type_to_check[2]; }
 		}
 
-		exchanger.publish(routing_key, {message: message, from: current_user, to: recipients}, {contentType: 'application/json'});
+		exchanger.publish(routing_key, {message: message, from: current_user.nickname, to: recipients}, {contentType: 'application/json'});
+	});
+
+	socket.on('transfer', function(data) {
+		var transfer_info = JSON.parse(data);
+		console.log(transfer_info);
+		current_user = socketUsers[socketConnections[socket.id]] ? socketUsers[socketConnections[socket.id]] : CHAT_DEFAULT_USER;
+		transfer_exchage.publish(current_user.current_room + '.#', {from: current_user.nickname, to: transfer_info.chat_room});
 	});
 
 	socket.on('disconnect', function() {
@@ -119,8 +147,7 @@ function handler(request, response) {
 			var urlParams = url.parse(request.url, true).query || {};
 
 			if(typeof urlParams.name != 'undefined'){
-				request.session.data.user = urlParams.name;
-				socketUsers[request.session.id] = request.session.data.user;
+				socketUsers[request.session.id] = {nickname: urlParams.name, current_room: 'someroom'};
 				response.writeHead(200, {
 					'Content-Type': 'text/html',
 					'Set-Cookie': [
